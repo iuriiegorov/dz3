c7749143cd4a7f0cd62bb34c45eb6ac5a61d16b0
Yuri E <196240@gmail.com>
2022-11-27 12:50:32 +0100

    ex15 added

---
+++ ex15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter a week day from 1 to 7");
+
+int day = Convert.ToInt32(Console.ReadLine());
+
+switch (day)
+{
+    case 6:
+        Console.WriteLine("Yes");
+        break;
+    case 7:
+        Console.WriteLine("Yes");
+        break;
+    default:
+        Console.WriteLine("No");
+        break;
+}
