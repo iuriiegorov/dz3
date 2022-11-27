c2ef822a7b445fd9b91ed419608f77f956448628
Yuri E <196240@gmail.com>
2022-11-27 11:30:25 +0100

    ex10 added

--- ex10/Program.cs
+++ ex10/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter a three-digit number");
+int N = Convert.ToInt32(Console.ReadLine());
+int M = ( N%100 - N%10 ) /10;
+Console.WriteLine (M);
+
